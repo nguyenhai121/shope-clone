e316e3014435804fe692871a6fbc4eb7fdce1900
ngochai <ngochai14051999@gmail.com>
2022-08-26 18:39:18 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,43 +1,93 @@
 import logo from './logo.svg';
 import './App.css';
 import ShoppingCard from './shoping-card'
+import ShoppeIcon from "./icon/shope-logo.svg";
+import { Line, Circle } from 'rc-progress';
+import { useState, useEffect } from "react";
 
 const listProductions = [
   {
-    name: 'computer'
+    name: 'computer',
+    manufacturer: 'Japan',
+    sold: 15,
+    key: 1
   }, 
   {
-    name: 'monitor'
+    name: 'monitor',
+    manufacturer: 'Usa',
+    sold: 14,
+    key: 2
   },
   {
-    name: 'motorbike'
+    name: 'motorbike',
+    manufacturer: 'Japan',
+    sold: 15,
+    key: 3
+  },
+  {
+    name: 'motorbike',
+    manufacturer: 'Japan',
+    sold: 15,
+    key: 4
+  },
+  {
+    name: 'motorbike',
+    manufacturer: 'Japan',
+    sold: 15,
+    key: 5
+  },
+  {
+    name: 'motorbike',
+    manufacturer: 'Japan',
+    sold: 15,
+    key: 6
+  },
+  {
+    name: 'motorbike',
+    manufacturer: 'Japan',
+    sold: 15,
+    key: 7
+  },
+  {
+    name: 'motorbike',
+    manufacturer: 'Japan',
+    sold: 15,
+    key: 8
+  },
+  {
+    name: 'motorbike',
+    manufacturer: 'Japan',
+    sold: 15,
+    key: 9
   }
 ]
 function App() {
+  const [count, setCount] = useState(0);
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setCount((count) => count + 5);
+  //   }, 1000);
+  // });
+  // handleOnClick() {
+  //   setCount((count) => count + 5)}
+  // }
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-    <div> 
+    <div style={{display: 'flex', flexDirection: 'row', width: '600px', flexWrap: 'wrap'}}> 
+      <Line percent={count} strokeWidth={4} strokeColor="blue" />
+      <h1>I've rendered {count} times!</h1>;
+      {/* <button onClick={()=> handleOnClick()}> 
+        Add
+      </button> */}
+      <img src={ShoppeIcon} />
       {
         listProductions.map((production) => {
-          return <ShoppingCard name={production.name} key={production.name}/>
+          return <ShoppingCard name={production.name} manufacturer={production.manufacturer} sold={production.sold} key={production.key}/>
         })
       }
+{/* 
+      <ShoppingCard name={listProductions[0].name} manufacturer={listProductions[0].manufacturer} sold={listProductions[0].sold} /> */}
     </div>
+
   );
 }
 
--- src/shoping-card.js
+++ src/shoping-card.js
@@ -1,7 +1,26 @@
 function ShoppingCard(props) {
     return (
-      <div>
-        {props.name}
+      <div onClick={() => alert('abc')} style={{ width: '100px', height: '200px', display: 'flex', flexDirection: 'column' ,marginRight: '20px', marginBottom: '20px'}}>
+        <div style={{flex: 2, background: 'green'}}> 
+
+        </div>
+        <div style={{flex: 1}}> 
+            <div> 
+                {
+                    props.name
+                }
+            </div>
+            <div> 
+                {
+                    props.manufacturer
+                }
+            </div>
+            <div> 
+                {
+                    props.sold
+                }
+            </div>
+        </div>
       </div>
     );
 }
